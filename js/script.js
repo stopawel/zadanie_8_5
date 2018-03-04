12628577aa13362a93e66405ee3b608cf3ef3a38
stopawel <36571721+stopawel@users.noreply.github.com>
2018-03-04 13:40:20 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 var a = 3;
 var h = 5;
-var triangleArea = a*h/2;
+var triangleArea = a * h / 2;
 
 console.log('triangle field whit base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
